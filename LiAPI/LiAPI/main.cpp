1ca466baef6b5c90db5521df9d9e317756d5048a
Anton <1802878@uad.ac.uk>
2021-02-03 13:33:53 +0000

    test
    format update test

---
+++ LiAPI/LiAPI/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  LiAPI
+//
+//  Created by Anton Sukhorukov on 03/02/2021.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+cout << "Hello, World!\n";
+    
+return 0;
+}
